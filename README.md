100dba5c5ac6dbe133dc19b834ed823103f17f96
zyberbob <spam@phantasmagoric.dk>
2026-06-30 15:21:08 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Manchester Ultimate Companion v3
+# MCR Companion v6
 
 Upload files to GitHub Pages repo root.
 
-Changes: Emily mode, removed offline mini-map, better map layers and tips.
+New: dashboard, flight/hotel fields, walking tours, food mode, dart mode, memory book export, journal photos, roulette.
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='manchester-ultimate-v3';
+const CACHE='mcr-companion-v6';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))});
